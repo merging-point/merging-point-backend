66f1a246c46a0355eff711fe4c8f0b6822ce24c6
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-05-23 01:50:13 +0900

    사용자 모델 추가

--- user/models.py
+++ user/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    username = models.CharField(max_length=30, null=False, unique=True)
+    disability_grade = models.IntegerField()
+    name = models.CharField(max_length=4)
+    phone_number = models.CharField(max_length=11, null=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('user')
+        ordering = ('-date_joined', )
+
+    def __str__(self):
+        return self.username
+
+    def is_staff(self):
+        return self.is_superuser
