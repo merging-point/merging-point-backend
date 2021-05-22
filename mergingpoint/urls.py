ad797edf26c1ea6654782b4891e1dabae20f8953
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-05-23 01:55:24 +0900

    swagger 문서 엔드포인트 추가

--- mergingpoint/urls.py
+++ mergingpoint/urls.py
@@ -14,10 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from rest_framework import permissions
 from django.urls import path
-from django.urls.conf import include
+from django.conf.urls import include, url
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(title="API Document", default_version="1"),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    url(
+        r'^swagger/(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json',
+    ),
+    url(
+        r'^swagger$',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
 ]
