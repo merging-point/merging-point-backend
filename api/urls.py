d94da8a50acd844a2a3c923985a3e05253684e50
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-05-23 01:54:39 +0900

    token 과 관련한 엔드포인트 추가

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from user.views import UserView
 from parkinglot.views import ParkinglotViewSet
 
 router = DefaultRouter(trailing_slash=False)
+router.register(r'users', UserView, basename="user")
 router.register(r'parkinglot', ParkinglotViewSet, basename="parkinglot")
 
-urlpatterns = []
+urlpatterns = [
+    path('token', TokenObtainPairView.as_view()),
+    path('token/refresh', TokenRefreshView.as_view()),
+    path('token/verify', TokenVerifyView.as_view()),
+]
 
 urlpatterns += router.urls
