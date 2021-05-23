1acc6316ff708e8a386af7d33dce7b4b6db2babc
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-05-23 10:56:16 +0900

    사용자 모델에 필드 추가 및 수정
    다음의 수정 사항:
    - disability_grade는 이제 Nullable 하지 않습니다.
    - name은 이제 Nullable 하지 않습니다.
    - phone_number은 이제 Nullable 하지 않습니다.
    - 사용자가 주차한 위치를 담는 IntegerField parked_at 필드가 추가되었습니다.

--- user/models.py
+++ user/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
+from parkinglot.models import Parkinglot
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     username = models.CharField(max_length=30, null=False, unique=True)
-    disability_grade = models.IntegerField()
-    name = models.CharField(max_length=4)
-    phone_number = models.CharField(max_length=11, null=True)
+    disability_grade = models.IntegerField(null=False)
+    name = models.CharField(max_length=4, null=False)
+    phone_number = models.CharField(max_length=11, null=False)
+    parked_at = models.IntegerField(default=-1, null=True)
     date_joined = models.DateTimeField(default=timezone.now)
     is_superuser = models.BooleanField(default=False)
 
