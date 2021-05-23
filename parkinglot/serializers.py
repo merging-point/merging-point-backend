b3bab87823731682dab313c4e6d9b576dce15aa1
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-05-23 11:02:21 +0900

    주차한 위치의 idx를 담기 위한 MarkParkinglotSerializer 추가

--- parkinglot/serializers.py
+++ parkinglot/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Parkinglot
 
 
+class MarkParkinglotSerailizer(serializers.Serializer):
+    parked_at = serializers.IntegerField()
+
+
 class ParkinglotSerializer(serializers.ModelSerializer):
     spots_for_disabled_cnt = serializers.IntegerField(read_only=True)
     avg_bigger_percentage = serializers.ReadOnlyField(read_only=True)
