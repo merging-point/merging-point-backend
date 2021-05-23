7db1919acbf6a25c58c7ef949e6b767b6477cbec
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-05-23 11:10:16 +0900

    주차중인 차 대수와 관련한 기능 추가

--- parkinglot/views.py
+++ parkinglot/views.py
@@ -1,13 +1,15 @@
 import json
 import math
 import requests
-from rest_framework import status, viewsets
+from django.db.models import Count
+from rest_framework import status, viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from user.models import User
 from .models import Parkinglot
-from .serializers import ParkinglotSerializer
+from .serializers import ParkinglotSerializer, MarkParkinglotSerailizer
 
 
 class Point:
@@ -59,9 +61,46 @@ class ParkinglotViewSet(viewsets.ViewSet):
         ),
     ]
 
+    idx_param = [
+        openapi.Parameter(
+            'idx',
+            openapi.IN_QUERY,
+            description="The idx of the parkinglot",
+            type=openapi.TYPE_NUMBER,
+        ),
+    ]
+
+    @action(
+        detail=False,
+        methods=['post'],
+        permission_classes=[permissions.IsAuthenticated],
+    )
+    @swagger_auto_schema(
+        manual_parameters=idx_param,
+        responses={200: MarkParkinglotSerailizer},
+    )
+    def mark_use(self, request):
+        idx = request.GET.get('idx')
+        request.user.parked_at = idx
+        request.user.save()
+        return Response({"parked_at": request.user.parked_at})
+
+    @action(
+        detail=False,
+        methods=['post'],
+        permission_classes=[permissions.IsAuthenticated],
+    )
+    @swagger_auto_schema(responses={200: MarkParkinglotSerailizer})
+    def mark_moved(self, request):
+        request.user.parked_at = -1
+        request.user.save()
+        return Response({"parked_at": request.user.parked_at})
+
     @action(detail=False, methods=['get'])
-    @swagger_auto_schema(manual_parameters=closest_param,
-                         responses={200: ParkinglotSerializer})
+    @swagger_auto_schema(
+        manual_parameters=closest_param,
+        responses={200: ParkinglotSerializer},
+    )
     def closest(self, request):
         # south west lat lng, north east lat lng
         required_parameters = [
@@ -69,6 +108,8 @@ class ParkinglotViewSet(viewsets.ViewSet):
             'south_west_longtitude', 'north_east_latitude',
             'north_east_longtitude'
         ]
+
+        # validate get parameters
         for required_parameter in required_parameters:
             if required_parameter not in request.GET:
                 return Response(
@@ -79,6 +120,8 @@ class ParkinglotViewSet(viewsets.ViewSet):
                     },
                     status=status.HTTP_400_BAD_REQUEST,
                 )
+
+        # three points
         center_point = Point(
             request.GET.get('center_latitude'),
             request.GET.get('center_longtitude'),
@@ -92,34 +135,42 @@ class ParkinglotViewSet(viewsets.ViewSet):
             request.GET.get('north_east_longtitude'),
         )
 
+        # get average
         temp = Parkinglot.objects.filter(
             parking_compartments_cnt__gte=50).only('parking_compartments_cnt')
-
         total = 0
         for t in temp:
             total += t.parking_compartments_cnt * 0.04
-
         avg = total / len(temp)
 
+        # get near parking lots
         found_parking_lots = Parkinglot.objects.filter(
             latitude__lte=north_east_point.latitude,
             latitude__gte=south_west_point.latitude,
             longtitude__lte=north_east_point.longtitude,
             longtitude__gte=south_west_point.longtitude,
         )
-
         serializer = ParkinglotSerializer(many=True, data=found_parking_lots)
         serializer.is_valid()
 
-        response_data = []
+        parked_info = {}
 
+        for x in User.objects.all().values('parked_at').annotate(
+                parked_cnt=Count("parked_at")).order_by('parked_at'):
+            parked_info[x['parked_at']] = x['parked_cnt']
+
+        # add the average bigger percentage
+        response_data = []
         for data in serializer.data:
             spots_for_disabled_cnt = data['parking_compartments_cnt'] * 0.04
             data['avg_bigger_percentage'] = round(
                 (spots_for_disabled_cnt - avg) / avg *
                 100)  # rounded percentage
+            data['parked_cnt'] = parked_info.get(data['idx'], 0)
             response_data.append(data)
+        # SELECT parked_at, COUNT(*) FROM user GROUP BY parked_at
 
+        # sort by short distance
         def calc_diagonal_distance(target_latitude, target_longtitude,
                                    center_latitude, center_longtitude):
             width = abs(float(target_latitude) - float(center_latitude))
@@ -131,8 +182,8 @@ class ParkinglotViewSet(viewsets.ViewSet):
             x['latitude'], x['longtitude'], center_point.latitude, center_point
             .longtitude), ), )  # sort by short distance
 
+        # add estimated time for the top 10 parking lots
         closest_top10 = response_data[:10]
-
         for i in range(len(closest_top10)):
             closest_top10[i]['estimated_time'] = get_arrival_duration(
                 f"{closest_top10[i]['longtitude']},{closest_top10[i]['latitude']}",
